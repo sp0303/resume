f554b4469775858c91e0fa4fb47de69015efaf05
sp0303 <66239077+sp0303@users.noreply.github.com>
2020-06-24 00:56:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from "react";
+import data from "./data.json";
+import Resume from "./resume";
+import profileIcon from "./static/media/person.svg";
+import profileIcon1 from "./static/media/person2.svg";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+
+// import SC from './StatesConcept';
+
+let App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/resume" component={Resume} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+let Home = () => {
+  let profiles = data.profiles;
+  return (
+    <div className="row justify-content-center" id="card-spl">
+      {profiles.map((value, index) => (
+        <div className="col-lg-4 col-md-4 col-sm-10 mt-1" key={index}>
+          {/* Card */}
+          <div
+            className="card card2"
+            style={{ boxShadow: "0px 8px 8px -8px #000" }}
+          >
+            <div className="card-body text-center">
+              {/* <img src={profileIcon} alt="Profile icon" style={{width:"30%"}}/> */}
+              {value.basics.gender === "male" ? (
+                <img
+                  src={profileIcon}
+                  alt="Profile icon"
+                  style={{ width: "30%" }}
+                />
+              ) : (
+                <img
+                  src={profileIcon1}
+                  alt="Profile icon"
+                  style={{ width: "30%" }}
+                />
+              )}
+              <h3> {value.basics.name} </h3>
+              <h5 className="text-secondary" style={{ fontStyle: "italic" }}>
+                {" "}
+                {value.basics.role}
+              </h5>{" "}
+              <hr />
+              <a href={"mailto:" + value.basics.email}>
+                {" "}
+                {value.basics.email}
+              </a>{" "}
+              <br></br>
+              <a href={"tel:" + value.basics.mobile}>
+                {" "}
+                {value.basics.mobile}
+              </a>{" "}
+              <br />
+              <Link
+                to={{ pathname: "/resume", data: { id: index } }}
+                className="btn btn-secondary text-white btn-block btn-sm"
+              >
+                View profile
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
